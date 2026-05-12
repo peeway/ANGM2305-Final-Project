c9f4cca380656077818509e5286991cd121654c1
Ethan Jimenez <emj240000@utdallas.edu>
2026-05-11 22:32:33 -0500

    added Colluision to blocks.

--- src/project.py
+++ src/project.py
@@ -27,6 +27,11 @@ class Grid:
         if row >= 0 and row < self.num_rows and column >= 0 and column < self.num_cols:
             return True
         return False
+    
+    def is_empty(self, row, column):
+        if self.grid[row][column] == 0:
+            return True
+        return False
 
     
     def draw(self, screen):
@@ -191,30 +196,38 @@ class Game:
     
     def move_left(self):
         self.current_block.move(0, -1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move (0, 1)
     
     def move_right(self):
         self.current_block.move(0, 1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move (0, -1)       
 
     def move_down(self):
         self.current_block.move(1,0)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move (-1,0)
+            self.lock_block()
 
     def lock_block(self):
         tiles = self.current_block.get_cell_positions()
         for position in tiles:
             self.grid.grid[position.row][position.column] = self.current_block.id
-        self.current_block = self.next_block()
+        self.current_block = self.next_block
         self.next_block = self.get_random_block()
+    
+    def block_fits(self):
+        tiles = self.current_block.get_cell_positions()
+        for tile in tiles:
+            if self.grid.is_empty(tile.row, tile.column) == False:
+                return False
+        return True
 
 
     def rotate(self):
         self.current_block.rotate()
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.undo_rotation()
 
 
